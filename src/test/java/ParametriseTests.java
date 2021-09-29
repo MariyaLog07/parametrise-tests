05c7a96ac9675b90189c1690b6481eb2975f2a9d
vladi <vladimirblokhin@yandex.ru>
2021-09-29 11:00:23 +0300

    add parametrize test with methodSource annotation

--- src/test/java/ParametriseTests.java
+++ src/test/java/ParametriseTests.java
@@ -1,14 +1,17 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.textCaseSensitive;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import domain.UnderlineMenuItems;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class ParametriseTests {
 
@@ -34,4 +37,26 @@ public class ParametriseTests {
         $(underlineMenuItems.getDesc()).click();
     }
 
+
+    static Stream<Arguments> githubLoginMethod() {
+        return Stream.of(
+                Arguments.of(
+                        "sggsdg", "111"
+                ),
+                Arguments.of(
+                        "ya.ru", "222"
+                )
+        );
+    }
+
+    @MethodSource("githubLoginMethod")
+    @ParameterizedTest(name = "{2}")
+    void githubLoginNegativeTest(String login, String password) {
+        open("https://github.com/login");
+        $("#login_field").sendKeys(login);
+        $("#password").sendKeys(password);
+        $(".btn-primary").click();
+        $(".flash-error").shouldBe(visible);
+
+    }
 }
